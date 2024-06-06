072549907aec854f7deba51fa22ae6d9c00730ec
MahimaMadhukar <107298868+MahimaMadhukar@users.noreply.github.com>
2024-06-06 09:09:46 +0530

    Expense Splitter is ready

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ document.getElementById("expense-form").addEventListener('submit',addExpense);
 
 let expenses = [];
 
-function addExpense(){
+function addExpense(event){
+    event.preventDefault();
     const description = document.getElementById("description").value;
     const amount = parseFloat(document.getElementById("amount").value);
     const paidBy = document.getElementById("paidBy").value;
@@ -22,8 +23,36 @@ function displayExpenses(){
     const expenseList = document.getElementById("expense-list");
     expenseList.innerHTML = '';
 
-    expenses.forEach(expense=>{const li = document.createElement('li');
+    expenses.forEach(expense=>
+        {const li = document.createElement('li');
         li.textContent = `${expense.description} - $${expense.amount} paid by ${expense.paidBy}, Split among ${expense.splitAmong.join(',')}`
+        expenseList.appendChild(li);
     });
+
+}
+
+function displaySummary(){
+const summary = expenses.reduce((acc,expense)=>{
+    const amountPerPerson = expense.amount / expense.splitAmong.length;
+    if (!acc[expense.paidBy]) acc[expense.paidBy] = 0;
+        acc[expense.paidBy] += expense.amount;
+
+        expense.splitAmong.forEach(person => {
+            if (!acc[person]) acc[person] = 0;
+            acc[person] -= amountPerPerson;
+        });
+
+        return acc;
+},{});
+
+const summaryList = document.getElementById('Summary-list');
+    summaryList.innerHTML = '';
+
+    Object.keys(summary).forEach(person => {
+        const li = document.createElement('li');
+        li.textContent = `${person}: $${summary[person].toFixed(2)}`;
+        summaryList.appendChild(li);
+    });
+
 }
 
